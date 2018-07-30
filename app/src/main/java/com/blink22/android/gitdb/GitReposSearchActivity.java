4e007eaf2764025cdad398df66a0d77d6318d899
Yusuf Hussein <youssef.hussin@blink22.com>
2018-07-30 15:51:53 +0200

    Initial commit

---
+++ app/src/main/java/com/blink22/android/gitdb/GitReposSearchActivity.java
@@ -0,0 +1,13 @@
+package com.blink22.android.gitdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitReposSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_repos_search);
+    }
+}
