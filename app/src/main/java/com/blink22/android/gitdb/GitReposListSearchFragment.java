ea143084b7ab4da711daf32dd0b910ddd2a74d47
Yusuf Hussein <youssef.hussin@blink22.com>
2018-07-30 18:43:15 +0200

    Add search action item

--- app/src/main/java/com/blink22/android/gitdb/GitReposListSearchFragment.java
+++ app/src/main/java/com/blink22/android/gitdb/GitReposListSearchFragment.java
@@ -1,9 +1,42 @@
 package com.blink22.android.gitdb;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GitReposListSearchFragment extends Fragment {
     public static GitReposListSearchFragment newInstance() {
         return new GitReposListSearchFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_git_repos_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
