29a38e6f38c3872f54017c1091ff996f08c0d0da
Yusuf Hussein <youssef.hussin@blink22.com>
2018-08-02 00:33:12 +0200

    Fix github api client to save a single instance of retrofit client

--- app/src/main/java/com/blink22/android/gitdb/GitHubApiClient.java
+++ app/src/main/java/com/blink22/android/gitdb/GitHubApiClient.java
@@ -9,6 +9,9 @@ public class GitHubApiClient {
     private static Retrofit retrofit = null;
 
     static Retrofit getClient() {
+        if (retrofit != null) {
+            return retrofit;
+        }
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
